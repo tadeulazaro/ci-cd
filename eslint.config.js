45a47760d12793336dd3888dc7503cc9ea9f280d
tadeuLazaro <tadeusf_sampo@hotmail.com>
2025-03-26 19:37:09 -0300

    decimo commit

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-unused-vars": "warn",
+        "eqeqeq": "error",
+        "indent": ["error", 2],
+        "semi": ["error", "always"]
+      }
+    }
+  ];
+  
