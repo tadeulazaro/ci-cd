963f91308c3069035d10fe283f95791e9ff6d33f
tadeuLazaro <tadeusf_sampo@hotmail.com>
2025-03-26 19:53:07 -0300

    decimo terceiro commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 module.exports = [
-  {
-    files: ["**/*.js"],
-    rules: {
-      "no-unused-vars": "warn",
-      "eqeqeq": "error",
-      "indent": ["error", 2],
-      "semi": ["error", "always"]
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-unused-vars": "warn",
+        "eqeqeq": "error",
+        "indent": ["error", 2],
+        "semi": ["error", "always"],
+        "no-undef": "error" // Adiciona erro para variáveis indefinidas
+      }
     }
-  }
-];
+  ];
   
