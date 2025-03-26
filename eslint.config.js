f6674f28df3bc9819200bacc2d16ea9a9eac7b5f
tadeuLazaro <tadeusf_sampo@hotmail.com>
2025-03-26 19:43:03 -0300

    decimo primeiro commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-export default [
-    {
-      files: ["**/*.js"],
-      rules: {
-        "no-unused-vars": "warn",
-        "eqeqeq": "error",
-        "indent": ["error", 2],
-        "semi": ["error", "always"]
-      }
+module.exports = [
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-unused-vars": "warn",
+      "eqeqeq": "error",
+      "indent": ["error", 2],
+      "semi": ["error", "always"]
     }
-  ];
+  }
+];
   
