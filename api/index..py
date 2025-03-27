382ae5bbb3ba187af52e545a14b8c0c689607ddb
tadeuLazaro <tadeusf_sampo@hotmail.com>
2025-03-27 08:50:25 -0300

    Adiciona CI/CD com Docker6 pyton flask

---
+++ api/index..py
@@ -0,0 +1,10 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response = {"message": "Olá, Vercel com Python!"}
+        self.wfile.write(json.dumps(response).encode('utf-8'))
